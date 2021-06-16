cd1ce2abef6af20ab908fd927d7ce5c11883f76d
Gerd Barodte <achsch@gmail.com>
2021-06-16 13:11:30 +0200

    Adressfelder hinzugefügt mit manueller ID

--- src/main/java/de/telekom/sea3/webserver/model/Person.java
+++ src/main/java/de/telekom/sea3/webserver/model/Person.java
@@ -2,17 +2,38 @@ package de.telekom.sea3.webserver.model;
 
 public class Person {
 	
+	private String id;
 	private String vorname;
 	private String nachname;
 	private String anrede;
+	private String str;
+	private String hausnr;
+	private String plz;
+	private String ort;
+	private String email;
 	
-	public Person(String vorname, String nachname, String anrede) {
+		
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Person(String id, String anrede, String vorname, String nachname, String str,  
+			String hausnr, String plz, String ort, String email ) {
+		this.id = id;
 		this.anrede = anrede;
 		this.vorname = vorname;
 		this.nachname = nachname;
+		this.str = str;
+		this.hausnr = hausnr;
+		this.plz = plz;
+		this.ort = ort;
+		this.email = email;
 		
-	}
-	
+		}
 	
 	public String getVorname() {
 		return vorname;
@@ -33,4 +54,47 @@ public class Person {
 		this.anrede = anrede;
 	}
 
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+
+	public String getHausnr() {
+		return hausnr;
+	}
+
+	public void setHausnr(String hausnr) {
+		this.hausnr = hausnr;
+	}
+
+	public String getPlz() {
+		return plz;
+	}
+
+	public void setPlz(String plz) {
+		this.plz = plz;
+	}
+
+
+	public String getOrt() {
+		return ort;
+	}
+
+
+	public void setOrt(String ort) {
+		this.ort = ort;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 }
--- src/main/resources/public/javaScript/myscript.js
+++ src/main/resources/public/javaScript/myscript.js
@@ -7,15 +7,22 @@ function getJson(irgendwas) { 	// irgendwas beinhaltet json mit allen kommunikat
 function getTxtFromJsonUndPackInsHTML(myjson) {
 	var tabelle = document.getElementById("tid001");
 	var i = 1;
-	for (var laufvariable of myjson.personen) {
+//	var id = 1;
+		for (var laufvariable of myjson.personen) {
 		// neue Zeile am Ende der exist. Tabelle anfügen
-		tabelle.insertAdjacentHTML("beforeend", 
+		tabelle.insertAdjacentHTML("beforeend",
 			"<tr>"
 			+ `<td> ${i++} </td>`
 			+ "<td><img src='" + getIcon(laufvariable.anrede) + "'></td>"
+			+ "<td>" + laufvariable.id + "</td>"			
 			+ "<td>" + laufvariable.anrede + "</td>"
 			+ "<td>" + laufvariable.vorname + "</td>"
 			+ "<td>" + laufvariable.nachname + "</td>"
+			+ "<td>" + laufvariable.str + "</td>"
+			+ "<td>" + laufvariable.hausnr + "</td>"
+			+ "<td>" + laufvariable.plz + "</td>"
+			+ "<td>" + laufvariable.ort + "</td>"
+			+ "<td>" + laufvariable.email + "</td>"
 			+ "</tr>")
 	}
 }
@@ -23,9 +30,9 @@ function getTxtFromJsonUndPackInsHTML(myjson) {
 // Einbau der Bilder
 function getIcon(anrede) {
 	switch (anrede) {
-		case "Herr":
+		case "mr":
 			return 'images/man.png'
-		case "Frau":
+		case "miss":
 			return 'images/frau.png'
 		default:
 			return 'images/frau.png'
@@ -36,13 +43,25 @@ function getIcon(anrede) {
 function oninputclick(event){
 	event.preventDefault();		// verhindert, ()GET request) dass das event weiter vom browser verarbeitet wird
 	console.log("click");
+	var id = document.getElementById("id").value;
+	console.log(vorname);
 	var vorname = document.getElementById("vorname").value;
 	console.log(vorname);
 	var nachname = document.getElementById("nachname").value;
 	console.log(nachname);
 	var anrede = document.getElementById("anrede").value;
 	console.log(anrede);
-	var jsondata=`{ "anrede": "${anrede}", "vorname": "${vorname}", "nachname": "${nachname}"}`;
+	var str = document.getElementById("str").value;
+	console.log(anrede);
+	var hausnr = document.getElementById("hausnr").value;
+	console.log(anrede);
+	var plz = document.getElementById("plz").value;
+	console.log(anrede);
+	var ort = document.getElementById("ort").value;
+	console.log(anrede);
+	var email = document.getElementById("email").value;
+	console.log(email);
+	var jsondata=`{ "id": "${id}","anrede": "${anrede}", "vorname": "${vorname}", "nachname": "${nachname}", "str": "${str}", "hausnr": "${hausnr}", "plz": "${plz}", "ort": "${ort}", "email": "${email}"}`;
 	console.log(jsondata);
 	
 	fetch("http://localhost:8080/json/person", {
@@ -61,3 +80,4 @@ input.addEventListener("click",oninputclick);
 fetch("http://localhost:8080/json/persons/all")
 	.then(getJson) 								//  entspricht: .then( irgendwas => irgendwas.json() )
 	.then(getTxtFromJsonUndPackInsHTML) 		// entpricht: cell.textContent = myjson.personen[0].vorname);
+	
