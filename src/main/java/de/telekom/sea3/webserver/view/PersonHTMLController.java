444f66ffd0488e860df67b2965a4dbe7f7d8b280
Gerd Barodte <achsch@gmail.com>
2021-06-17 14:47:23 +0200

    GetAllPersons über tymeleaf integriert - keine Formatierung

--- src/main/java/de/telekom/sea3/webserver/view/PersonHTMLController.java
+++ src/main/java/de/telekom/sea3/webserver/view/PersonHTMLController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import de.telekom.sea3.webserver.model.Personen;
 import de.telekom.sea3.webserver.service.PersonService;
 
 @Controller  // liefert HTML und kein Json
@@ -16,7 +17,7 @@ public class PersonHTMLController {
 
 	private static final String HTMLTEMPLATE = "<!DOCTYPE html> <html lang='de'> "
 			+ "<head> <meta charset='utf-8'> <meta name='viewport' content='width=device-width, "
-			+ "initial-scale=1.0'> <title>Titel</title> </head> <body>size: %d </body> </html>";
+			+ "initial-scale=1.0'> <title>Titel</title> </head> <body><h1>size: %d </h1></body> </html>";
 
 	private PersonService personService; // Variable personService
 
@@ -30,14 +31,14 @@ public class PersonHTMLController {
 
 	// URL: "http://localhost:8080/size"
 	@GetMapping("/size")
-	@ResponseBody
+	@ResponseBody   // sagt dem Spring Framework, dass die Antwort in den Body gepackt werden soll und nicht über das Template
 	public String getSize() {
 		String string5 = String.format(HTMLTEMPLATE, personService.getSize());
 		return string5;
 	}
 	
 	// URL:"http://localhost:8080/count"
-		@GetMapping("/count")
+		@GetMapping("/count")   // kein @ResponseBody, da es sonst den Weg über das Template verhindert.
 		public String getCount(Model model, 
 				@RequestParam(value = "name",   // Aufruf mit "http://localhost:8080/count/?name=Gerd" 
 						required = false, 
@@ -51,11 +52,18 @@ public class PersonHTMLController {
 		
 		
 		// URL:"http://localhost:8080/size2"
-		@GetMapping("/size2")  // Kein @ResponseBody, da es ... verhindert
+		@GetMapping("/size2")  // kein @ResponseBody, da es sonst den Weg über das Template verhindert.
 		public String getSize2(Model model) {
 			model.addAttribute("size3", personService.getSize());
 			return "size";
 		}
 		
+		// URL:"http://localhost:8080/size2"
+		@GetMapping("/personen")  // kein @ResponseBody, da es sonst den Weg über das Template verhindert.
+		public String getPersonen(Model model) {
+			Personen personen = personService.getAllPersons();
+			model.addAttribute("personenList", personen.getPersonen());
+			return "personen";
+		}
 	
 }
